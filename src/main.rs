ef851b82544b14e9a59398168adaca44225f8773
falkecarlsen <11318702+falkecarlsen@users.noreply.github.com>
2025-01-09 16:09:00 +0100

    add scheduled kick routine

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
+#![allow(unreachable_code)]
+
 extern crate ev3dev_lang_rust;
 
 use core::time::Duration;
-use ev3dev_lang_rust::motors::{MediumMotor, MotorPort};
+use ev3dev_lang_rust::motors::{MediumMotor, MotorPort, TachoMotor};
 use ev3dev_lang_rust::sensors::{ColorSensor, SensorPort};
 use ev3dev_lang_rust::Ev3Result;
 use std::sync::{Arc, RwLock};
 use std::thread;
 
-#[derive(PartialEq, Debug, Copy, Clone)]
-#[derive(strum_macros::Display)]
+#[derive(PartialEq, Debug, Copy, Clone, strum_macros::Display)]
 enum BrickColor {
     None = 0,
     Black,
@@ -35,7 +36,7 @@ impl GetColorEnumTrait for ColorSensor {
             Ok(5) => BrickColor::Red,
             Ok(6) => BrickColor::White,
             Ok(7) => BrickColor::Brown,
-            _ => BrickColor::None
+            _ => BrickColor::None,
         }
     }
 }
@@ -68,8 +69,11 @@ impl SensorDebouncer {
             self.readings.remove(0);
         }
         // most likely brick is the one that has been detected most frequently, use filter on readings to get most common
-        self.most_likely_brick = *self.readings.iter()
-            .rev().take(self.window_size)
+        self.most_likely_brick = *self
+            .readings
+            .iter()
+            .rev()
+            .take(self.window_size)
             .max_by_key(|&x| self.readings.iter().filter(|&y| *y == *x).count())
             .unwrap_or(&BrickColor::None);
     }
@@ -84,21 +88,65 @@ impl SensorDebouncer {
     }
 }
 
+fn run_to_abs_pos(motor: TachoMotor, position: i32, speed: i32) -> Ev3Result<()> {
+    // check direction to go
+    let current_pos = motor.get_position()?;
+    if current_pos < position {
+        motor.set_duty_cycle_sp(speed)?;
+    } else {
+        motor.set_duty_cycle_sp(-speed)?;
+    }
+
+    motor.wait(
+        || motor.get_position().unwrap() >= position,
+        Some(Duration::from_secs(5)),
+    );
+    motor.stop()?;
+    Ok(())
+}
+
+fn schedule_timed_piston(kicker: TachoMotor, duration: Duration, brick_color: BrickColor) -> Ev3Result<()> {
+    // encode direction (left, right, or no kick) by scalar
+    let direction = match brick_color {
+        BrickColor::None => 0,
+        BrickColor::Black => 0,
+        BrickColor::Blue => -1,
+        BrickColor::Green => 0,
+        BrickColor::Yellow => 1,
+        BrickColor::Red => -1,
+        BrickColor::White => 0,
+        BrickColor::Brown => 0,
+    };
+    //
+    let time_now = std::time::Instant::now();
+    // do prime operation
+    run_to_abs_pos(kicker.clone(), 90 * direction, 20)?;
+
+    // delay for duration
+    while time_now.elapsed() < duration {
+        thread::sleep(Duration::from_millis(10));
+    }
+
+    // kick
+    run_to_abs_pos(kicker.clone(), -90 * direction, 20)?;
+
+    Ok(())
+}
+
+
 fn main() -> Ev3Result<()> {
     println!("Hello, EV3!");
     // Get large motor on port outA.
     let conveyor = MediumMotor::get(MotorPort::OutA)?;
-    let kicker = MediumMotor::get(MotorPort::OutB)?;
+    let kicker = TachoMotor::get(MotorPort::OutB)?;
 
     conveyor.run_direct()?;
     conveyor.set_duty_cycle_sp(45)?;
 
+    // reset kicker rotary encoder position (ensure piston is aimed at center of conveyer on init)
+    let _first_pos = kicker.set_position(0)?;
+
     kicker.run_direct()?;
-
-    // reset kicker rotary encoder position
-    let _first_pos = kicker.set_position_sp(0)?;
-    kicker.set_duty_cycle_sp(-30)?;
-
 
     let s1 = ColorSensor::get(SensorPort::In1)?;
     let s2 = ColorSensor::get(SensorPort::In2)?;
@@ -109,16 +157,19 @@ fn main() -> Ev3Result<()> {
     let debouncer_s1 = Arc::new(RwLock::new(SensorDebouncer::new(10)));
     let debouncer_s2 = Arc::new(RwLock::new(SensorDebouncer::new(10)));
 
-
     // thread for reading sensor data and running debouncer
     let debouncer_sensor_s1 = Arc::clone(&debouncer_s1);
     let debouncer_sensor_s2 = Arc::clone(&debouncer_s2);
-    let debouncer_sensor_handle = thread::spawn(move || {
-        loop {
-            debouncer_sensor_s1.write().unwrap().update(s1.get_color_enum());
-            debouncer_sensor_s2.write().unwrap().update(s2.get_color_enum());
-            std::thread::sleep(Duration::from_millis(16));
-        }
+    let debouncer_sensor_handle = thread::spawn(move || loop {
+        debouncer_sensor_s1
+            .write()
+            .unwrap()
+            .update(s1.get_color_enum());
+        debouncer_sensor_s2
+            .write()
+            .unwrap()
+            .update(s2.get_color_enum());
+        std::thread::sleep(Duration::from_millis(16));
     });
 
     // thread for main loop only reading from debouncer
@@ -126,11 +177,17 @@ fn main() -> Ev3Result<()> {
     let s2_debounce_reader = Arc::clone(&debouncer_s2);
     let main_handle = thread::spawn(move || {
         loop {
-            //println!("s1: {:?}, s2: {:?}", s1.get_color_enum(), s2.get_color_enum());
-            println!("Most likely bricks: s1: {: <8} s2: {: <8}",
-                     s1_debounce_reader.read().unwrap().get_most_likely_brick(),
-                     s2_debounce_reader.read().unwrap().get_most_likely_brick());
-            //println!("Readings: {:?}", debouncer_reader.read().unwrap().get_readings());
+            println!(
+                "Most likely bricks: s1: {: <8} s2: {: <8}",
+                s1_debounce_reader.read().unwrap().get_most_likely_brick(),
+                s2_debounce_reader.read().unwrap().get_most_likely_brick()
+            );
+            if s2_debounce_reader.read().unwrap().get_most_likely_brick() != BrickColor::None {
+                let brick_color = s2_debounce_reader.read().unwrap().get_most_likely_brick();
+                println!("Kicking a brick: {:?}", brick_color);
+                schedule_timed_piston(kicker.clone(), Duration::from_secs(3), brick_color).unwrap();
+            };
+
             std::thread::sleep(Duration::from_millis(200));
         }
     });
